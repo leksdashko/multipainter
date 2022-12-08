4800cdd8dceb2fbb12108bbf65acbd9de9af27ad
Oleksandr Dashko <leksdashko@gmail.com>
2022-12-07 22:37:42 -0800

    undo redo

--- src/components/SettingBar.jsx
+++ src/components/SettingBar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import toolState from '../store/toolState';
 
 const SettingBar = () => {
 	return (
-		<div className="setting-bar">SettingBar</div>
+		<div className="setting-bar">
+			<label>Line width:</label>
+			<input 
+				onChange={e => toolState.setLineWidth(e.target.value)}
+				style={{margin: '0 10px'}}
+				id="line-width"
+				type="number"
+				defaultValue={1} min={1} max={50}
+			/>
+		</div>
 	)
 }
 
--- src/components/Toolbar.jsx
+++ src/components/Toolbar.jsx
@@ -4,19 +4,26 @@ import toolState from '../store/toolState';
 import '../styles/toolbar.scss';
 import Brush from '../tools/Brush';
 import Circle from '../tools/Circle';
+import Eraser from '../tools/Eraser';
 import Rect from '../tools/Rect';
 
 const Toolbar = () => {
+
+	const changeColor = e => {
+		toolState.setStrokeColor(e.target.value);
+		toolState.setFillColor(e.target.value);
+	}
+
 	return (
 		<div className="toolbar">
 			<button className='toolbar__btn brush' onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
 			<button className='toolbar__btn rect' onClick={() => toolState.setTool(new Rect(canvasState.canvas))}/>
 			<button className='toolbar__btn circle' onClick={() => toolState.setTool(new Circle(canvasState.canvas))}/>
-			<button className='toolbar__btn eraser'/>
+			<button className='toolbar__btn eraser' onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}/>
 			<button className='toolbar__btn line'/>
-			<input type="color"/>
-			<button className='toolbar__btn undo'/>
-			<button className='toolbar__btn redo'/>
+			<input onChange={e => changeColor(e)} type="color"/>
+			<button className='toolbar__btn undo' onClick={() => canvasState.undo()}/>
+			<button className='toolbar__btn redo' onClick={() => canvasState.redo()}/>
 			<button className='toolbar__btn save'/>
 		</div>
 	)
